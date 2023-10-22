f5f15ac83e999a2e13ef2cc2cae4e4ec52e6329d
James Rucker <james@rucker.land>
2023-10-22 16:53:16 -0700

    Add eslint-config-prettier so ESLint does not conflict with Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
